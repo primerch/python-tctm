924fb25638db59051e8f5cde967254d32e32c744
Reacher <macryze@icloud.com>
2024-10-31 16:44:11 +0800

    Improve battery level check with user input
    Updated the initial power value to be input by the user. Added an else statement to notify if the battery level is high. This enhances the practice exercise by making it interactive and more comprehensive.

--- complete-code/lesson04/practice/practice01.py
+++ complete-code/lesson04/practice/practice01.py
@@ -2,6 +2,13 @@
 # 使用if语句模拟手机在电量小于百分之二十时，就发出“电量低”的提醒功能，假设初始电量为10%
 # 提示1：百分之二十的大小为0.2
 # 提示2：电量可用power表示
-power = 0.1
+# power = 0.1
+# if power < 0.2:
+#     print('电量低')
+
+power = float(input())
+
 if power < 0.2:
     print('电量低')
+else:
+    print('电量高')
